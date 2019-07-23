7cb5d7a6e98950f900494d265df81ea9c1c2e0ea
CrumpetFullerton <53240251+CrumpetFullerton@users.noreply.github.com>
2019-07-23 18:49:24 -0500

    Add files via upload

---
+++ IdleSRC/Assets/FantasyMonster/Skeleton/Character/BehaviourScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourScript : MonoBehaviour
+{
+    public Transform player;
+    static Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator> ();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 direction = player.position - this.transform.position;
+        float angle = Vector3.Angle(direction,this.transform.forward);
+        if (Vector3.Distance(player.position, this.transform.position) < 25 && angle < 90)
+        {
+           
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp
+                (this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+            anim.SetBool("isIdle", false);
+            if (direction.magnitude > 8)
+            {
+                this.transform.Translate(0, 0, 0.25f);
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isAttacking", false);             
+
+            }
+            else if(direction.magnitude < 10)
+            {
+                direction = player.position - this.transform.position;
+                direction.y = 0;
+
+                this.transform.rotation = Quaternion.Slerp
+                    (this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+                anim.SetBool("isIdle", false);
+                if (direction.magnitude > 2)
+                {
+                    this.transform.Translate(0, 0, 0.03f);
+                    anim.SetBool("isWalking", true);
+                    anim.SetBool("isAttacking", false);
+                    anim.SetBool("isRunning", false);
+
+                }
+                else
+                {
+                    anim.SetBool("isWalking", false);
+                    anim.SetBool("isAttacking", true);
+                    anim.SetBool("isRunning", false);
+                }
+            }
+            else
+            {
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isWalking",false);
+                anim.SetBool("isAttacking", false);
+                anim.SetBool("isRunning", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", false);
+            anim.SetBool("isRunning", false);
+
+        }
+
+    }
+}
---
+++ IdleSRC/Assets/FantasyMonster/Skeleton/Character/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    static Animator anim;
+    public float speed = 10.0f;
+    public float rotationSpeed = 75.0f;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Verticle") * speed;
+        float straffe = Input.GetAxis("horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetButton("Fire1"))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        else
+            anim.SetBool("isAttacking", false);
+        if(translation != 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isIdle", false);
+
+        }
+        else { anim.SetBool("isWalking", false);anim.SetBool("isIdle", true); }
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+    }
+}
